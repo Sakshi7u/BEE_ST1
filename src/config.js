18ce96cfb291c37cb790c825e5fcaf884862b4d0
Sakshi7u <113376624+Sakshi7u@users.noreply.github.com>
2023-11-29 17:06:31 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+config();
+
+export const PORT = process.env.PORT || 4000;
+export const MONGODB_URI = "mongodb+srv://Sakshi7:Sakshi13@cluster0.nlaeoj1.mongodb.net/";
